7026442bb6164ec61dc03c3772a890a7c736c4f1
zeo <zeo29021992@gmail.com>
2021-09-05 16:55:45 +0530

    Improve perf

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,22 +1,45 @@
 <script lang="ts">
-	import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'sveltestrap';
+	import {
+		Collapse,
+		Navbar,
+		NavbarToggler,
+		NavbarBrand,
+		Nav,
+		NavItem,
+		NavLink,
+		Dropdown,
+		DropdownToggle,
+		DropdownMenu,
+		DropdownItem
+	} from 'sveltestrap';
 
 	let isOpen: boolean = false;
+	let isDropdownOpen: boolean = false;
 	function handleUpdate(event) {
 		isOpen = event.detail.isOpen;
 	}
 </script>
 
-<Navbar color="primary" dark expand="md">
+<Navbar color="primary" dark expand="md" style="background-color: #e3f2fd;">
 	<NavbarBrand href="/">My Photo Gallery</NavbarBrand>
 	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
 	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
 		<Nav class="ms-auto" navbar>
 			<NavItem>
-				<NavLink class="active" href="/">Photo Albums</NavLink>
+				<NavLink class="active" href="/">Wedding</NavLink>
 			</NavItem>
+			<Dropdown nav isOpen={isDropdownOpen} toggle={() => (isDropdownOpen = !isDropdownOpen)}>
+				<DropdownToggle nav caret>Other Albums</DropdownToggle>
+				<DropdownMenu>
+					<DropdownItem header>Albums</DropdownItem>
+					<DropdownItem disabled>Action</DropdownItem>
+					<DropdownItem>First</DropdownItem>
+					<DropdownItem divider />
+					<DropdownItem href="/post">Second</DropdownItem>
+				</DropdownMenu>
+			</Dropdown>
 			<NavItem>
-				<NavLink href="/post">Posts</NavLink>
+				<NavLink href="/post">Blog Posts</NavLink>
 			</NavItem>
 			<NavItem>
 				<NavLink href="/pricing">Pricing</NavLink>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,15 +4,11 @@
 </script>
 
 <svelte:head>
-	<link
-		rel="stylesheet"
-		href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.1/styles/github.min.css"
-	/>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Overpass" />
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fira+Mono" />
+	<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Overpass" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fira+Mono" /> -->
 </svelte:head>
 
-<div class="g-app-wrapper container-sm">
+<div class="g-app-wrapper">
 	<Nav />
 	<slot />
 </div>
