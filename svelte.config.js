4c26276a1b2e7a437f5c88bad85494532cabd8a0
Prakash Borkar <zeo29021992@gmail.com>
2021-09-05 14:31:55 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import mdsvexConfig from "./mdsvex.config.js"
 import adapterStatic from "@sveltejs/adapter-static"
 import sveltePreprocess from "svelte-preprocess"
 import path from "path"
-
+// comment
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   extensions: [".svelte", ...mdsvexConfig.extensions],
